3bb1a91c071141fff34a926d0209fbbe2ca25f29
Shirlen Detablan <slen.detablan@gmail.com>
2020-09-16 19:32:26 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,13 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg;
+}
 /* Write your implementation of displayMessage() */
+function greet(timeStr) {
+  const time = parseInt(timeStr, 10);
+  if (time > 16) return "Good Evening"
+  if (time < 11) return "Good Morning"
+  return "Good Afternoon"
+}
